4698d219d5ffa2350fedfcdc726d88b04ceccbd8
Gabriel La Torre <hola@latorregabriel.com>
2017-04-17 20:34:22 -0300

    footer corregido

--- sitio/footer.php
+++ sitio/footer.php
@@ -1,44 +1,63 @@
-		<footer id="footer">
-			<img src="img/trama5.jpg" >
-  	</footer>
-	
-
-
-
+<footer id="footer">
     <div class="container">
           <div class="row">
-          
+            <div class="col-md-7 col-md-offset-1">
+                <div class="row fondo-blanco">
+                    <div class="col-md-4">
+                        <img id="logo-footer" src="images/logofooter.png">
+                    </div>
+                    <div class="col-md-8 letra-negra">
+                        <div class="row">
+                            <div class="col-md-12">
+                            <h2>AMYRIS S.A</h2>
+                            </div>
+                        </div>
+                        <div class="row dato-footer">
+                            <div class="col-md-1">
+                                <i class="fa fa-map-marker "></i>
+                            </div>
+                            <div class="col-md-11">
+                                Cajaraville 2902 y/o Curapaligue 6510, Munro(1605), Pcia de Bs. As.
+                            </div>
+                        </div>
+                        <div class="row dato-footer">
+                            <div class="col-md-1">
+                                <i class="fa fa-envelope"></i>
+                            </div>
+                            <div class="col-md-11">
+                                ventas@amirys.com.ar
+                            </div>
+                        </div>
+                        <div class="row dato-footer">
+                            <div class="col-md-1">
+                                <i class="fa fa-phone"></i>
+                            </div>
+                            <div class="col-md-11">
+                                5197-1918
+                            </div>
+                        </div>
 
-
-            <div class="col-md-7" id="logofooter">
-                <img id="logofooterimagen" src="images/logofooter.png">
-               <h3 >2016 - Todos los derechos reservados // Web Design ODM Brandig</h3>
-
-                <div id="listadentro" >
-                <h2>AMYRIS S.A</h1>
-                    <ul >
-                    <li> <i class="fa fa-map-marker ">  Curapaligue 6510, Munro(1605)<br>pcia de BSAS</i></li>
-                      <li><i class="fa fa-envelope"> ventas@amiry.com.ar</i></li>
-                      <li><i class="fa fa-phone"> 5197-1918</i></li>
-               
-                    </ul> 
+                    </div>
+                    <div class="col-md-10 col-md-offset-1">
+                        <h6 >2016 - Todos los derechos reservados // Web Design ODM Brandig</h6>
+                    </div>
                 </div>
-
+            </div>
+            <div class="col-md-2" id="lista-afuera" >
+              <ul>
+                  <a href="quienes-somos"> <li><?php echo $amyris ?></li></a>
+                  <a href="fragancias"> <li><?php echo $fragancias ?></li></a>
+                  <a href="expertise"> <li><?php echo $expertise ?></li></a>
+                  <a href="sustentabilidad"> <li><?php echo $sustentabilidad ?></li></a>
+                  <a href="innovaciones"> <li><?php echo $innovaciones ?></li></a>
+                  <a href="contacto"> <li><?php echo $contacto2 ?></li></a>
+              </ul>
+            </div>
             </div>
 
 
-          <div class="col-md-4" id="listaafuera" >
-             <ul >
-               <a href="quienes-somos"> <li><?php echo $amyris ?></li></a>
-               <a href="fragancias"> <li><?php echo $fragancias ?></li></a>
-               <a href="expertise"> <li><?php echo $expertise ?></li></a>
-               <a href="sustentabilidad"> <li><?php echo $sustentabilidad ?></li></a>
-               <a href="innovaciones"> <li><?php echo $innovaciones ?></li></a>
-               <a href="contacto"> <li><?php echo $contacto2 ?></li></a>
-             </ul> 
 
-          </div>
+        </div>
+    </div>
 
-
-</div>
-</div>
+</footer>
