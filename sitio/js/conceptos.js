8e33d54c3949ea8da5b213012f968cfa3e1c4e19
Gabriel La Torre <hola@latorregabriel.com>
2017-05-03 11:56:47 -0300

    animaciones de la isla de concepto a medio hacer

--- sitio/js/conceptos.js
+++ sitio/js/conceptos.js
@@ -1,16 +1,33 @@
+var timeOutId = null;
+
 $(document).ready(function(){
 
     var entraMouse = function(event) {
-        var element = event.currentTarget;
-        $(element).addClass('selected');
-        $(element).siblings().addClass('not-selected');
+        timeOutId = setTimeout(
+            function (event) {
+                var element = event.currentTarget;
+                $(element).addClass('selected');
+                $(element).siblings().removeClass('selected');
+                $(element).siblings().addClass('not-selected');
+                timeOutId = null;
+            },
+            300,
+            event
+        );
+
 
     };
 
     var saleMouse = function(event) {
-        var element = event.currentTarget;
-        $(element).removeClass('selected');
-        $(element).siblings().removeClass('not-selected');
+        setTimeout(function(event) {
+            if (timeOutId != null || !$(event.currentTarget).hasClass('selected')) {
+                return;
+            }
+            var element = event.currentTarget;
+            $(element).removeClass('selected');
+            $(element).siblings().removeClass('not-selected');
+        }, 300, event);
+
     };
 
    $(".isla-background").hover(entraMouse, saleMouse);
