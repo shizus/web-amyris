bf99cf4f96f1cfebb0cb27726d67a2abd5095cd6
Gabriel La Torre <hola@latorregabriel.com>
2017-05-05 00:43:15 -0300

    arreglo el footer

--- sitio/footer.php
+++ sitio/footer.php
@@ -1,6 +1,12 @@
 <?php
 
+
+require_once 'requirelanguage.php';
+
 function footer($containerId, $fotoContainerId, $titulo, $texto){
+    
+    global $amyris, $fragancias, $expertise, $sustentabilidad, $innovaciones, $contacto2;
+    
     echo "
     <div class='container' id='$containerId'>
         <div class='row'>
@@ -18,69 +24,65 @@ function footer($containerId, $fotoContainerId, $titulo, $texto){
         </div>
     </div>
     
-    <footer id=\"footer\">
-    <div class=\"container\">
-          <div class=\"row\">
-            <div class=\"col-md-7 col-md-offset-1 col-sm-12 col-sm-offset-0\">
-                <div class=\"row fondo-blanco\">
-                    <div class=\"col-md-5\">
-                        <img id=\"logo-footer\" src=\"images/logofooter.png\">
+    <footer id='footer'>
+    <div class='container'>
+          <div class='row'>
+            <div class='col-md-7 col-md-offset-1 col-sm-12 col-sm-offset-0'>
+                <div class='row fondo-blanco'>
+                    <div class='col-md-5'>
+                        <img id='logo-footer' src='images/logofooter.png'>
                     </div>
-                    <div class=\"col-md-7 letra-negra\">
-                        <div class=\"row\">
-                            <div class=\"col-md-12\">
+                    <div class='col-md-7 letra-negra'>
+                        <div class='row'>
+                            <div class='col-md-12'>
                             <h2>AMYRIS S.A.</h2>
                             </div>
                         </div>
-                        <div class=\"row dato-footer\">
-                            <div class=\"col-md-1\">
-                                <i class=\"fa fa-map-marker \"></i>
+                        <div class='row dato-footer'>
+                            <div class='col-md-1'>
+                                <i class='fa fa-map-marker '></i>
                             </div>
-                            <div class=\"col-md-11\">
+                            <div class='col-md-11'>
                                 Cajaraville 2902 y/o Curapaligue 6510, Munro (1605), Pcia de Bs. As.
                             </div>
                         </div>
-                        <div class=\"row dato-footer\">
-                            <div class=\"col-md-1\">
-                                <i class=\"fa fa-envelope\"></i>
+                        <div class='row dato-footer'>
+                            <div class='col-md-1'>
+                                <i class='fa fa-envelope'></i>
                             </div>
-                            <div class=\"col-md-11\">
+                            <div class='col-md-11'>
                                 ventas@amyris.com.ar
                             </div>
                         </div>
-                        <div class=\"row dato-footer\">
-                            <div class=\"col-md-1\">
-                                <i class=\"fa fa-phone\"></i>
+                        <div class='row dato-footer'>
+                            <div class='col-md-1'>
+                                <i class='fa fa-phone'></i>
                             </div>
-                            <div class=\"col-md-11\">
+                            <div class='col-md-11'>
                                 54 11 5197-1918
                             </div>
                         </div>
 
                     </div>
-                    <div class=\"col-md-10 col-md-offset-1\">
+                    <div class='col-md-10 col-md-offset-1'>
                         <h6 >2017 - Todos los derechos reservados //
-                            <a href=\"odmbranding.com\" target=\"_blank\" class=\"footer-link\">Web Design ODM Branding</a></h6>
+                            <a href='odmbranding.com' target='_blank' class='footer-link'>Web Design ODM Branding</a></h6>
                     </div>
                 </div>
             </div>
-            <div class=\"col-md-2\" id=\"lista-afuera\" >
+            <div class='col-md-2' id='lista-afuera' >
               <ul>
-                  <a href=\"quienes-somos\"> <li>$amyris </li></a>
-                  <a href=\"fragancias\"> <li>$fragancias </li></a>
-                  <a href=\"expertise\"> <li>$expertise </li></a>
-                  <a href=\"sustentabilidad\"> <li>$sustentabilidad </li></a>
-                  <a href=\"innovaciones\"> <li>$innovaciones </li></a>
-                  <a href=\"contacto\"> <li>$contacto2 </li></a>
+                  <a href='quienes-somos'> <li>$amyris </li></a>
+                  <a href='fragancias'> <li>$fragancias </li></a>
+                  <a href='expertise'> <li>$expertise </li></a>
+                  <a href='sustentabilidad'> <li>$sustentabilidad </li></a>
+                  <a href='innovaciones'> <li>$innovaciones </li></a>
+                  <a href='contacto'> <li>$contacto2 </li></a>
               </ul>
             </div>
             </div>
-
-
-
         </div>
     </div>
-
 </footer>
     ";
 }
