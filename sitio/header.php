c1c6927914ed1598f614d03981c7f096b3c9f089
Gabriel La Torre <hola@latorregabriel.com>
2017-05-10 11:42:00 -0300

    cambios en expertise

--- sitio/header.php
+++ sitio/header.php
@@ -58,6 +58,33 @@
     <!-- ekko-lightbox.min -->
     <script src="js/lightbox.js"></script>
     <script src="js/menu-desplegable.js"></script>
+        <script type="text/javascript">
+            // to top right away
+            if ( window.location.hash ) scroll(0,0);
+            // void some browsers issue
+            setTimeout( function() { scroll(0,0); }, 1);
+
+            $(function() {
+
+                // your current click function
+                $('.scroll').on('click', function(e) {
+                    e.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: ($($(this).attr('href')).offset().top-80)+ 'px'
+                    }, 1000, 'swing');
+                });
+
+                // *only* if we have anchor on the url
+                if(window.location.hash) {
+
+                    // smooth scroll to the anchor id
+                    $('html, body').animate({
+                        scrollTop: ($(window.location.hash).offset().top-80) + 'px'
+                    }, 1000, 'swing');
+                }
+
+            });
+        </script>
 
     </head>
 <body>
