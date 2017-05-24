1deb318f1f20e0039dda61963290d517fa879c8d
Gabriel La Torre <hola@latorregabriel.com>
2017-05-24 16:27:02 -0300

    Arreglo el slider para ipad y iphone

--- sitio/slider_index.php
+++ sitio/slider_index.php
@@ -12,10 +12,10 @@
                         <h2 class="wow fadeInDown animated primero centered slider-size" id="titulo1" ><?php echo $bienvenidos ?></h2>
                         <h2 class="wow fadeInDown animated primero centered slider-size" id="titulo2"><?php echo $amyris ?></h2>
                         <div class="row">
-                            <div class="col-md-4 col-md-offset-2">
+                            <div class="col-md-4 col-md-offset-2 col-xs-12 col-xs-offset-0">
                                 <h2 class="wow fadeInDown animated segundo slider-size slogan" id="titulo3"><?php echo $creamos1 ?></h2>
                             </div>
-                            <div class="col-md-4">
+                            <div class="col-md-4 col-xs-12 col-xs-offset-0">
                                 <h2 class="wow fadeInDown animated tercero slider-size slogan" id="titulo4" ><?php echo $creamos2 ?></h2>
                             </div>
 
@@ -57,7 +57,7 @@
         </nav>
         -->
 
-        <nav id="nav-arrows" class="nav-arrows hidden-xs hidden-sm visible-md visible-lg">
+        <nav id="nav-arrows" class="nav-arrows visible-xs visible-sm visible-md visible-lg">
             <a href="javascript:;" class="sl-prev">
                 <i class="fa fa-angle-left fa-3x"></i>
             </a>
@@ -65,7 +65,7 @@
                 <i class="fa fa-angle-right fa-3x"></i>
             </a>
         </nav>
-        <nav id="nav-dots" class="nav-dots visible-xs visible-sm hidden-md hidden-lg">
+        <nav id="nav-dots" class="nav-dots hidden-xs hidden-sm hidden-md hidden-lg">
             <span class="nav-dot-current"></span>
             <span></span>
             <span></span>
