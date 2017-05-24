c3e589b8866d9a7bdbcba663184a5bc5576c5109
Gabriel La Torre <hola@latorregabriel.com>
2017-05-24 17:49:06 -0300

    mejoro la isla de conceptos

--- sitio/conceptos.php
+++ sitio/conceptos.php
@@ -1,4 +1,4 @@
-<div class="container conceptos hide-until-dom-ready">
+<div class="container conceptos hide-until-dom-ready hidden-sm hidden-xs visible-lg visible-md">
     <div class="row">
         <div class="content col-md-12">
             <div class="row islas-container">
@@ -60,5 +60,66 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+
+<div class="container-fluid conceptos hide-until-dom-ready visible-sm visible-xs hidden-lg hidden-md">
+                <div class="sentir row">
+
+                    <div class="concepto-titulo-container col-sm-12">
+                        <h4 class="concepto-titulo">
+                          <?php echo $isla_sentir_titulo ?>
+                        </h4>
+
+                    </div>
+                    <div class="concepto-texto-container col-sm-12">
+                        <div class="concepto-texto">
+                            <span><?php echo $isla_sentir_texto ?></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="descubrir row">
+
+                    <div class="concepto-titulo-container col-sm-12">
+                        <h4 class="concepto-titulo">
+                          <?php echo $isla_descubrir_titulo ?>
+                        </h4>
+
+                    </div>
+                    <div class="concepto-texto-container col-md-12">
+                        <div class="concepto-texto">
+                            <span><?php echo $isla_descubrir_texto ?></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="crear row">
+
+                    <div class="concepto-titulo-container col-sm-12">
+                        <h4 class="concepto-titulo">
+                          <?php echo $isla_crear_titulo ?>
+                        </h4>
+
+                    </div>
+                    <div class="concepto-texto-container col-sm-12">
+                        <div class="concepto-texto">
+                            <span><?php echo $isla_crear_texto ?></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="disfrutar row">
+
+                    <div class="concepto-titulo-container col-sm-12">
+                        <h4 class="concepto-titulo">
+                          <?php echo $isla_disfrutar_titulo ?>
+                        </h4>
+
+                    </div>
+
+                    <div class="concepto-texto-container col-sm-12">
+                        <div class="concepto-texto">
+                            <span><?php echo $isla_disfrutar_texto ?></span>
+                        </div>
+                    </div>
+                </div>
     </div>
 </div>
