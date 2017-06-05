8f2c27ce3a27a0904990f91533d8169a06bd782e
Gabriel La Torre <hola@latorregabriel.com>
2017-06-05 19:22:26 -0300

    video y sustentabilidad

--- sitio/js/video.js
+++ sitio/js/video.js
@@ -1,6 +1,11 @@
 //var domain = "http://localhost:8080";
 var domain = window.location.href;
-var origen = domain + '/media/iStock_48438620_HD_1080_webm_5mb.webm';
+var origen = domain + '/media/iStock_48438620_HD_1080-1280x720-ipad.mp4';
+var isMobile = navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry)/);
+if (isMobile) {
+    origen = domain + '/media/iStock_48438620_HD_1080-640x360-iphone.mp4';
+}
+// var origen = domain + '/media/iStock_48438620_HD_1080_webm_5mb.webm';
 var settings = {autoplay: 'autoplay',
                 muted: 'muted',
                 loop: 'loop',
