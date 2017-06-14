0ebcb596685e48d9ae46c225571b009169d36232
Gabriel La Torre <hola@latorregabriel.com>
2017-06-14 17:04:50 -0300

    corrigiendo menú desplegable en tablet, en progreso

--- sitio/menu-desplegable.php
+++ sitio/menu-desplegable.php
@@ -58,57 +58,67 @@
   <div class="container-fluid visible-sm visible-xs hidden-lg hidden-md">
     <div class="row">
       <div class="col-sm-12 amyris-nav-container center">
-          <div style="height: 6em"></div>
-      <img src="images/flor-menu.png">
-        <h1 class="nav-bar-title">
-            <a href="quienes-somos">Amyris</a>
-        </h1>
-        <div class="link-container"><a href="quienes-somos#quienes-somos">¿Quienes Somos?</a></div>
-        <div class="link-container"><a href="quienes-somos#valores">Valores</a></div>
-        <div class="link-container"><a href="quienes-somos#porqueamir">¿Por qué Amyris?</a></div>
+          <div class="menu-option-container">
+              <div style="height: 6em"></div>
+              <img src="images/flor-menu.png">
+              <h1 class="nav-bar-title">
+                  <a href="quienes-somos">Amyris</a>
+              </h1>
+              <div class="link-container"><a href="quienes-somos#quienes-somos">¿Quienes Somos?</a></div>
+              <div class="link-container"><a href="quienes-somos#valores">Valores</a></div>
+              <div class="link-container"><a href="quienes-somos#porqueamir">¿Por qué Amyris?</a></div>
+          </div>
       </div>
     </div>
     <div class="row">
       <div class="col-sm-12 fragancias-nav-container center">
-      <img src="images/flor-menu.png">
-        <h1 class="nav-bar-title">
-            <a href="fragancias">Fragancias</a>
-        </h1>
-        <div class="link-container"><a href="fragancias#sistema-conceptual">Sistema Conceptual</a></div>
-        <div class="link-container"><a href="fragancias#productos">Productos</a></div>
+          <div class="menu-option-container">
+              <img src="images/flor-menu.png">
+              <h1 class="nav-bar-title">
+                  <a href="fragancias">Fragancias</a>
+              </h1>
+              <div class="link-container"><a href="fragancias#sistema-conceptual">Sistema Conceptual</a></div>
+              <div class="link-container"><a href="fragancias#productos">Productos</a></div>
+          </div>
       </div>
     </div>  
     <div class="row">
       <div class="col-sm-12 expertise-nav-container center">
-      <img src="images/flor-menu.png">
-        <h1 class="nav-bar-title">
-            <a href="expertise">Expertise</a>
-        </h1>
-        <div class="link-container"><a href="expertise">Desarrollo y Creación</a></div>
-        <div class="link-container"><a href="expertise#infraestructura">Infraestructura</a></div>
-        <div class="link-container"><a href="expertise#control-calidad">Control de Calidad</a></div>
-        <div class="link-container"><a href="expertise#comercio-exterior">Comercio exterior</a></div>
+          <div class="menu-option-container">
+              <img src="images/flor-menu.png">
+              <h1 class="nav-bar-title">
+                  <a href="expertise">Expertise</a>
+              </h1>
+              <div class="link-container"><a href="expertise">Desarrollo y Creación</a></div>
+              <div class="link-container"><a href="expertise#infraestructura">Infraestructura</a></div>
+              <div class="link-container"><a href="expertise#control-calidad">Control de Calidad</a></div>
+              <div class="link-container"><a href="expertise#comercio-exterior">Comercio exterior</a></div>
+          </div>
       </div>
     </div>  
     <div class="row">
       <div class="col-sm-12 sustentabilidad-nav-container center">
-      <img src="images/flor-menu.png">
-        <h1 class="nav-bar-title">
-            <a href="sustentabilidad">Sustentabilidad</a>
-        </h1>
-        <div class="link-container"><a href="sustentabilidad#valores">Valores</a></div>
-        <div class="link-container"><a href="sustentabilidad#sustentabilidad">Enfoque Sustentable</a></div>
-        <div class="link-container"><a href="sustentabilidad#como-operamos">¿Cómo operamos?</a></div>
+          <div class="menu-option-container">
+              <img src="images/flor-menu.png">
+              <h1 class="nav-bar-title">
+                  <a href="sustentabilidad">Sustentabilidad</a>
+              </h1>
+              <div class="link-container"><a href="sustentabilidad#valores">Valores</a></div>
+              <div class="link-container"><a href="sustentabilidad#sustentabilidad">Enfoque Sustentable</a></div>
+              <div class="link-container"><a href="sustentabilidad#como-operamos">¿Cómo operamos?</a></div>
+          </div>
       </div>
     </div>
     <div class="row">
       <div class="col-sm-12 innovaciones-nav-container center">
-      <img src="images/flor-menu.png">
-        <h1 class="nav-bar-title">
-            <a href="innovaciones">Innovaciones</a>
-        </h1>
-        <div class="link-container"><a href="innovaciones#innovar">Aprendemos para innovar</a></div>
-        <div class="link-container"><a href="innovaciones#ideas">Departamento de ideas, desarrollo e innovación</a></div>
+          <div class="menu-option-container">
+              <img src="images/flor-menu.png">
+              <h1 class="nav-bar-title">
+                  <a href="innovaciones">Innovaciones</a>
+              </h1>
+              <div class="link-container"><a href="innovaciones#innovar">Aprendemos para innovar</a></div>
+              <div class="link-container"><a href="innovaciones#ideas">Departamento de ideas, desarrollo e innovación</a></div>
+          </div>
       </div>
     </div>
     <div class="row visible-xs visible-sm hidden-lg hidden-md contacto-row" style="background-color: #D1BE87">
